5f19fed25f7b99e032bb02d27a6ebbd8209c9548
Saulo Oliveira <sauslash@hotmail.com>
2020-02-23 16:40:49 -0300

    initial commit

---
+++ src/webparts/home/components/AppForm/App.tsx
@@ -0,0 +1,88 @@
+//3 conceitos principais do react
+//Componente - Bloco isolado de html, css e js, o qual não interfere no restante da aplicação
+//Propriedades - Informações que o componente pai passa para o componente filho
+//Estado - Informações mantidas pelo componente (lembrar imutabilidade)
+
+// <> - conceito do fragment, para renderizar varios componentes dentro do return sem usar uma div por exemplo
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+import DevItem from '../DevItem';
+import DevForm from '../DevForm';
+import { sp } from '@pnp/sp';
+
+export const App: React.FunctionComponent = () => { 
+
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {      
+      sp.web.lists
+        .getByTitle("Devs")
+        .select("ID, name, githubUsername, techs, avatarUrl, bio")
+        .items.top(5000)
+        .get()
+        .then(items => {          
+          setDevs(items);
+        },
+        (err) => {
+          console.log(err);
+        });      
+    }
+
+    loadDevs();
+
+  }, []);
+
+  async function handleAddDev(data) {
+
+    const { github_username, techs, latitude, longitude } = data;
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    const { name, avatar_url, bio } = apiResponse.data;
+    
+    data.name = name;
+    data.avatarUrl = avatar_url;
+    data.bio = bio;
+    data.githubUsername = github_username;
+    
+    sp.web.lists.getByTitle("Devs").items.add({
+      githubUsername: github_username,
+      name: name,
+      avatarUrl: avatar_url,
+      bio: bio,
+      techs: techs,
+      latitude: latitude,
+      longitude: longitude
+    }).then(i => {          
+        console.log(i);
+        setDevs([...devs, data]);
+    },
+    (err) => {
+      console.log(err);            
+    });
+
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev.ID} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+
+};
